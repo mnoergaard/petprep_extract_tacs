4376ebb98b9e05407608861b8d78b279ed6de504
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2024-05-28 11:12:07 -0500

    freeze local test scripts

--- test_run_locally.sh
+++ test_run_locally.sh
@@ -4,4 +4,4 @@ source petprep_extract_tacs_env/bin/activate
 BIDS_DIR=/home/galassiae/Data/sharing/test2
 OUTPUT_DIR=$BIDS_DIR/derivatives/petprep_extract_tacs
 
-python3 run.py --bids_dir $BIDS_DIR --output_dir $OUTPUT_DIR --wm --n_procs 6 --petprep_hmc
+strace -o $(date '+%Y-%m-%d_%H:%M:%S')_local_strace.txt python3 run.py --bids_dir $BIDS_DIR --output_dir $OUTPUT_DIR --wm --n_procs 6 --petprep_hmc
