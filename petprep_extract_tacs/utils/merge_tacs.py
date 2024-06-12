0dd14478eddb43531c7d7364f5405e00bc542fc6
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2024-06-11 20:54:51 -0400

    merges tacs, need to update to merge the other run tsv's

--- petprep_extract_tacs/utils/merge_tacs.py
+++ petprep_extract_tacs/utils/merge_tacs.py
@@ -2,6 +2,12 @@ import pandas
 import pathlib
 import bids
 import argparse
+import os
+from difflib import get_close_matches
+from pprint import pprint
+import fnmatch
+import re
+import glob
 
 from petprep_extract_tacs.bids import collect_participants
 
@@ -29,7 +35,7 @@ def offset_to_one_time_zero(bids_dir, *tacs, inplace=False):
     layout = bids.BIDSLayout(bids_dir, derivatives=True)
 
 
-def collect_and_merge_tacs(bids_dir, subject='', session='', **kwargs):
+def collect_and_merge_tacs(bids_dir, **kwargs):
     """
     Collect and merge all TACs from a BIDS directory or BIDS subject directory.
 
@@ -37,12 +43,6 @@ def collect_and_merge_tacs(bids_dir, subject='', session='', **kwargs):
     ----------
     bids_dir : str
         Path to the BIDS directory.
-    subject : str
-        Subject label.
-    session : str
-        Session label.
-    run : str
-        Run label.
     **kwargs : dict
         Keyword arguments to pass to pandas.read_csv.
 
@@ -51,21 +51,53 @@ def collect_and_merge_tacs(bids_dir, subject='', session='', **kwargs):
     pandas.DataFrame
         Collected TACs.
     """
-    layout = bids.BIDSLayout(bids_dir, derivatives=True)
-    if subject == '':
-        participants = collect_participants(bids_dir)
-        tacs = []
-        for subject in participants:
-            tacs.append(collect_and_merge_tacs(bids_dir, f"sub-{subject}", session, **kwargs))
-        merge_tacs(tacs, **kwargs)
-    elif subject != '' and session != '':
-        tacs = layout.get(subject=subject, suffix='tac', extension='tsv', return_type='file')
-        tacs = merge_tacs(*tacs, **kwargs)
-    elif subject != '' and session != '':
-        # get a list of all the sessions per subject and merge those tacs session by session
-        tacs = layout.get(subject=subject, session=session, suffix='tac', extension='tsv', return_type='file')
-    return tacs
+    
+    # get every file in the bids directory with a tacs suffix
+    all_tacs = []
+    for root, folder, files in os.walk(bids_dir):
+        for f in files:
+            if 'tacs' in f:
+                all_tacs.append(os.path.join(root, f))
+    
+    all_tacs.sort()
+    
+    # create a unix style * string for each tac file by replacing the part of the 
+    # path "_run-01_" with "_run-*_" instead.
+    # this will allow us to use fnmatch to find all of the files that match the pattern
+    unix_style_tacs = [re.sub(r'_run-[0-9]_', '_run-[0-9]_', f) for f in all_tacs]
+    unix_style_tacs = list(set(unix_style_tacs))
+    unix_style_tacs.sort()
+    
+    # now we make sure that the regex string we made for each unix style tac can handle the "." and "/" characters
+    unix_style_tacs = [re.sub(r'\.', r'\.', f) for f in unix_style_tacs] # escape .
+    unix_style_tacs = [re.sub(r'/', r'\/', f) for f in unix_style_tacs] # escape /
 
+    # now we use glob to find all of the files that match the pattern
+    to_be_merged_tacs = {}
+
+    for t in unix_style_tacs:
+        to_be_merged_tacs[t] = []
+        for tac in all_tacs:
+            if re.match(t, tac):
+                to_be_merged_tacs[t].append(tac)
+    for values in to_be_merged_tacs.values():
+        print("These tacs will be merged:")
+        for v in values:
+            print(v)
+    values = list(to_be_merged_tacs.values())
+
+    merged_tacs = []
+    for v in values:
+            tacs = merge_tacs(*v)
+            # create a filename for these merged tacs by removing the _run-[0-9] from the filename
+            # this will allow us to save the merged tacs to a single file
+            combined_file_name = re.sub(r'_run-[0-9]_', '_', v[0])
+            tacs.to_csv(combined_file_name, sep='\t', index=False, **kwargs)
+            merged_tacs.append(combined_file_name)
+            for t in v:
+                os.remove(t)
+
+    return merge_tacs
 
 def merge_tacs(*args, **kwargs):
     """
@@ -87,23 +119,16 @@ def merge_tacs(*args, **kwargs):
     args = [pathlib.Path(arg).resolve() for arg in args if pathlib.Path(arg).exists()]
 
     for arg in args:
-        tac = pandas.read_csv(arg, **kwargs)
+        tac = pandas.read_csv(arg, sep='\t', **kwargs)
         tacs.append(tac)
     tacs = pandas.concat(tacs, ignore_index=True)
     return tacs
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Merge TACs from different files into a single file.')
-    parser.add_argument('--bids_dir', type=str, help='Path to the BIDS directory.')
-    parser.add_argument('--subject', type=str, help='Subject label.')
-    parser.add_argument('--session', type=str, help='Session label.')
-    parser.add_argument('--tacs', type=str, nargs='+', help='TAC file paths to merge.')
-    parser.add_argument('--output', type=str, help='Output file path.')
+    parser.add_argument('bids_dir', type=str, help='Path to the BIDS directory.')
     args = parser.parse_args()
 
-    if not args.tacs:
-        tacs = collect_and_merge_tacs(args.bids_dir, args.subject, args.session)
-    else:
-        tacs = merge_tacs(*args.tacs)
-    tacs.to_csv(args.output, sep='\t', index=False)
+    tacs = collect_and_merge_tacs(args.bids_dir)
+
 
