a27b6b7e5876d6c14478dbca8db7b0e47b314f48
Martin Norgaard <martin.noergaard@nru.dk>
2024-06-17 11:57:21 +0200

    FIX: update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name='petprep_extract_tacs',
     version='0.0.1',
     packages=find_packages(),
-    package_dir={"": "petprep_extract_tacs"},
     url='https://github.com/mnoergaard/petprep_extract_tacs',
     author='Martin Norgaard',
     author_email='martin.noergaard@di.ku.dk',
