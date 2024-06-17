22fec13773fdf4660ea8ba8abb14fa89371a6b25
Martin Norgaard <martin.noergaard@nru.dk>
2024-06-17 16:42:50 +0200

    ENH: add updated setup and dist

--- setup.py
+++ setup.py
@@ -2,8 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='petprep_extract_tacs',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'petprep_extract_tacs=petprep_extract_tacs.run:main',
+        ],
+    },
     url='https://github.com/mnoergaard/petprep_extract_tacs',
     author='Martin Norgaard',
     author_email='martin.noergaard@di.ku.dk',
