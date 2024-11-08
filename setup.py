ec3597ff20bf1e80cce24cc9541165b13804937b
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2024-11-08 14:08:21 -0600

    formatted with black

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='petprep_extract_tacs',
-    version='0.0.2',
+    name="petprep_extract_tacs",
+    version="0.0.2",
     packages=find_packages(),
     entry_points={
-        'console_scripts': [
-            'petprep_extract_tacs=petprep_extract_tacs.run:main',
+        "console_scripts": [
+            "petprep_extract_tacs=petprep_extract_tacs.run:main",
         ],
     },
-    url='https://github.com/mnoergaard/petprep_extract_tacs',
-    author='Martin Norgaard',
-    author_email='martin.noergaard@di.ku.dk',
-    description='PETPrep Extraction of Time Activity Curves'
+    url="https://github.com/mnoergaard/petprep_extract_tacs",
+    author="Martin Norgaard",
+    author_email="martin.noergaard@di.ku.dk",
+    description="PETPrep Extraction of Time Activity Curves",
 )
